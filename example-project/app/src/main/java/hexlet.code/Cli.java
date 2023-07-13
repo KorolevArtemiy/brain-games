f617ab9fd7647135b41f60300ac8e64a58036af8
KorolevArtemiy <art55544@gmail.com>
2023-07-14 00:25:53 +0300

    Add Cli

--- example-project/app/src/main/java/hexlet.code/Cli.java
+++ example-project/app/src/main/java/hexlet.code/Cli.java
@@ -1,4 +1,11 @@
 package hexlet.code;
-
+import java.util.Scanner;
 public class Cli {
+    public static void name() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+        scanner.close();
+    }
 }
