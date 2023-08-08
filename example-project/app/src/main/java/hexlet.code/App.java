6837af5c354156eadfa69a5cde8c5d7cee243465
KorolevArtemiy <art55544@gmail.com>
2023-08-08 23:14:31 +0300

    Add checkstyle.xml

--- example-project/app/src/main/java/hexlet.code/App.java
+++ example-project/app/src/main/java/hexlet.code/App.java
@@ -1,8 +1,15 @@
 package hexlet.code;
-
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.name();
+        Scanner scanner = new Scanner(System.in);
+        String userChoice = scanner.next();
+        System.out.println("Please enter the game number and press Enter. \n 1 - Greet \n 0 - Exit\n");
+        System.out.println("Your choice: " + userChoice );
+        if (userChoice.equals("1")) {
+            System.out.println("Welcome to the Brain Games!");
+            Cli.name();
+
+        }
     }
 }
