aadac78386e79ed3d3ac25a3d61b845b13a7a5d8
KorolevArtemiy <art55544@gmail.com>
2023-08-17 06:27:18 +0300

    Add Engine.java

---
+++ example-project/app/src/main/java/hexlet.code/Engine.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+public class Engine {
+    public static Boolean wrong(String ask, String ans) {
+        if (ask.equals(ans)) {
+            System.out.println("Correct!");
+            return false;
+        } else {
+            System.out.println("'" + ans + "' is wrong answer ;(. Correct answer was '" + ask + "'"
+                    + "\nLet's try again, " + Cli.userName + "!");
+            return true;
+        }
+    }
+}
