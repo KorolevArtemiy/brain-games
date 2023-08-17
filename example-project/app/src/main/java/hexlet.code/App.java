729ce86729c2bc27613fee5a1ed91c8246fc27fd
KorolevArtemiy <art55544@gmail.com>
2023-08-17 06:26:41 +0300

    Add Calc.java

--- example-project/app/src/main/java/hexlet.code/App.java
+++ example-project/app/src/main/java/hexlet.code/App.java
@@ -1,4 +1,7 @@
 package hexlet.code;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Even;
+
 import java.util.Scanner;
 
 public class App {
@@ -6,6 +9,7 @@ public class App {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
+        System.out.println("3 - Calc");
         System.out.println("0 - Exit");
         System.out.print("Your choice: ");
 
@@ -15,7 +19,8 @@ public class App {
             Cli.name();
         }
         switch (userChoice) {
-            case 2 -> Even.game();
+            case 2 -> Even.game2();
+            case 3 -> Calc.game3();
             default -> {
             }
         }
