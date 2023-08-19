2275c487deeeaf61fcafe6cafaab447f539958a0
KorolevArtemiy <art55544@gmail.com>
2023-08-19 06:38:12 +0300

    Rename

---
+++ app/src/main/java/hexlet.code/games/GCD.java
@@ -0,0 +1,34 @@
+package hexlet.code.games;
+
+import java.util.Scanner;
+
+public class GCD {
+    public static void game4() {
+        System.out.println("Find the greatest common divisor of given numbers.");
+        Scanner scanner = new Scanner(System.in);
+
+        for (int q = 1; q <= 3; q++) {
+            int value1 = 1 + (int) (Math.random() * 100);
+            int value2 = 1 + (int) (Math.random() * 100);
+            int min = Math.min(value1, value2);
+            int max = Math.max(value1, value2);
+            int gcd = min;
+
+            while (gcd != 0) {
+                gcd = max % min;
+                max = min;
+                min = gcd;
+            }
+
+            System.out.println("Question " + q + ": " + value1 + "  " + value2);
+            System.out.print("Your answer: ");
+            String userAnswer = scanner.next();
+
+            String ask = String.valueOf(max);
+
+            if (hexlet.code.Engine.wrong(ask, userAnswer)) {
+                break;
+            }
+        }
+    }
+}
---
+++ app/src/main/java/hexlet.code/games/Progression.java
@@ -0,0 +1,35 @@
+package hexlet.code.games;
+
+import java.util.Scanner;
+
+public class Progression {
+    public static void game5() {
+        System.out.println("What number is missing in the progression?");
+        Scanner scanner = new Scanner(System.in);
+
+        for (int q = 1; q <= 3; q++) {
+            int qua = 5 + (int) (Math.random() * 10);
+            int step = 2 + (int) (Math.random() * 10);
+            int first = 2 + (int) (Math.random() * 50);
+            int skip = (int) (Math.random() * qua) * step + first;
+            int max = qua * step + first;
+            String ask = String.valueOf(skip);
+            System.out.print("Question " + q + ": ");
+
+            for (int j = first; j <= max; j += step) {
+                if (j == skip) {
+                    System.out.print(".. ");
+                } else {
+                    System.out.print(j + " ");
+                }
+            }
+
+            System.out.print("\nYour answer: ");
+            String userAnswer = scanner.next();
+
+            if (hexlet.code.Engine.wrong(ask, userAnswer)) {
+                break;
+            }
+        }
+    }
+}
