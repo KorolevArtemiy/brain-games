91b6809d29eb5f2b4be627f37f7382cf78c6171b
KorolevArtemiy <art55544@gmail.com>
2023-08-19 03:57:57 +0300

    Update Prime.java

--- example-project/app/src/main/java/hexlet.code/games/Prime.java
+++ example-project/app/src/main/java/hexlet.code/games/Prime.java
@@ -6,10 +6,10 @@ public class Prime {
     public static void game6() {
         System.out.println("Answer 'yes' if given number is prime. Otherwise answer 'no'.");
         Scanner scanner = new Scanner(System.in);
-        String ask = "yes";
 
         for (int q = 1; q <= 3; q++) {
             int value = (int) (Math.random() * 100);
+            String ask = "yes";
             if (((value % 2 != 0) && (value > 2)) || (value == 2)) {
                 for (int i = 3; i < value; i += 2) {
                     if (value % i == 0) {
