bed8f151bfd0d7082d7278bcc9b97547f34d5821
KorolevArtemiy <art55544@gmail.com>
2023-08-19 03:47:28 +0300

    Update Even.java

--- example-project/app/src/main/java/hexlet.code/games/Even.java
+++ example-project/app/src/main/java/hexlet.code/games/Even.java
@@ -6,15 +6,13 @@ public class Even {
     public static void game2() {
         System.out.println("Answer 'yes' if the number is even, otherwise answer 'no'.");
         Scanner scanner = new Scanner(System.in);
-        String ask;
+        String ask = "no";
 
         for (int q = 1; q <= 3; q++) {
             int value = (int) (Math.random() * 100);
 
             if (value % 2 == 0) {
                 ask = "yes";
-            } else {
-                ask = "no";
             }
 
             System.out.println("Question " + q + ": " + value);
