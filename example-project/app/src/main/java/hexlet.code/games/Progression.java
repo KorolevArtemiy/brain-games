2eda7ac939fda485b8c2140ac806afe6649ed1d5
KorolevArtemiy <art55544@gmail.com>
2023-08-19 04:12:28 +0300

    Update Progression.java

--- example-project/app/src/main/java/hexlet.code/games/Progression.java
+++ example-project/app/src/main/java/hexlet.code/games/Progression.java
@@ -10,16 +10,15 @@ public class Progression {
         for (int q = 1; q <= 3; q++) {
             int qua = 5 + (int) (Math.random() * 10);
             int step = 2 + (int) (Math.random() * 10);
-            int skip = (int) (Math.random() * qua);
             int first = 2 + (int) (Math.random() * 50);
-            String ask = "";
-
+            int skip = (int) (Math.random() * qua) * step + first;
+            int max = qua * step + first;
+            String ask = String.valueOf(skip);
             System.out.print("Question " + q + ": ");
 
-            for (int j = first; j <= first + qua * step; j += step) {
-                if (j == first + skip * step) {
+            for (int j = first; j <= max; j += step) {
+                if (j == skip) {
                     System.out.print(".. ");
-                    ask = String.valueOf(j);
                 } else {
                     System.out.print(j + " ");
                 }
